77e29d3cf8dbe059817839d33a30fd7c9c0c9f92
Roger Esteve <rogeresteve30@gmail.com>
2023-02-06 21:13:09 +0100

    clean up

--- example/example.py
+++
@@ -1,8 +0,0 @@
-# cargamos la plantilla
-f = open("ex_snippet.html", "r")
-data = f.read()
-
-version = "80D" # ejemplo de variable extraida del .conf
-data = data.replace("%var1%", str(version))
-data = data.replace("%var2%", "suda el valor")
-
