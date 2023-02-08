6a3769ca56a0e82d2cf0fd3b1861541ba5b8b8ce
Roger Esteve <rogeresteve30@gmail.com>
2023-02-08 19:15:20 +0100

    fill snippet: 1.1-2.5
    Co-Authored-By: ezixd23 <57570015+ezixd23@users.noreply.github.com>

--- main.py
+++ main.py
@@ -7,8 +7,16 @@ def load_conf(path):
 
     conf = {}
 
-    i = 0
+    header = raw_data[:4]
+    conf_version = header[0].split("=")[1].split("-")
+    version = conf_version[0][-3:]
+    op=conf_version[4].split(":")[0]
+    firm = f"v{conf_version[1]},{conf_version[3]} ({op})"
 
+    conf['#version'] = version
+    conf['#firm'] = firm
+
+    i = 0
     current_stack = []
 
     def set_conf_based_on_stack(key, val):
@@ -35,7 +43,91 @@ def load_conf(path):
 
     return conf
 
+def fill_snippet(snippet, conf):
+    set_var = lambda var, val: snippet.replace("%"+var+"%", val)
 
+    # 1.1
+    snippet = set_var('config-version', conf['#version'])
+    snippet = set_var('version', conf['#version'])
+    snippet = set_var('firm', conf['#firm'])
+
+    #2.2
+    port = conf['config system global']['admin-sport'][0]
+    snippet = set_var('acc',port)
+    
+    conf_admin = conf['config system admin']['edit "admin"']
+    if 'trusthost1' in conf_admin:
+        ip1 = conf_admin['trusthost1']
+        ip2 = conf_admin['trusthost2']
+        ip3 = conf_admin['trusthost3']
+        snippet = set_var('ip1', ip1[0])
+        snippet = set_var('ip2', ip2[0])
+        snippet = set_var('ip3', ip3[0])
+
+    #2.3
+    prima = conf['config system dns']['primary'][0]
+    secon = conf['config system dns']['secondary'][0]
+    domain = conf['config system dns']['domain'][0][1:-1]
+    snippet = set_var('spri', prima)
+    snippet = set_var('ssec', secon)
+    snippet = set_var('dom', domain)
+    
+    
+    # 2.4
+    table_interf = ""
+    interf = conf['config system interface']
+    for key in interf.keys():
+        name = key[6:-1]
+        if 'ip' in interf[key]:
+            table_interf += "<tr>"
+            table_interf += f"<td>{name}</td>"
+            
+            if "alias" in interf[key]:
+                table_interf +=f"<td>" + interf[key]["alias"][0][1:-1] + "</td>"
+            else:
+                table_interf +="<td>-</td>"
+            table_interf += "<td>" + interf[key]["ip"][0] + "</td>"
+
+            if "dhcp-relay-ip" in interf[key]:
+                table_interf+= "<td>" + interf[key]["dhcp-relay-ip"][0][1:-1] + "</td>"
+            else:
+                table_interf +="<td>-</td>"
+            table_interf +="</tr>"
+    snippet = set_var("table_interf", table_interf)
+
+    #2.5
+    count = len(conf['config router static'])
+    router_static = conf['config router static']
+
+    table_enr = ""
+    for key in router_static.keys():
+        table_enr += "<tr>"
+        
+        # Xarxa destí
+        if "dst" in router_static[key]:
+            table_enr +=f"<td>" + router_static[key]["dst"][0] + "/" + router_static[key]["dst"][1] + "</td>"
+        else:
+            table_enr +="<td>0.0.0.0/0.0.0.0</td>"
+
+        # GW
+        if "gateway" in router_static[key]:
+            table_enr +=f"<td>" + router_static[key]["gateway"][0] + "</td>"
+        else:
+            table_enr +="<td>0.0.0.0</td>"
+
+        # Interficie
+        table_enr +=f"<td>" + router_static[key]["device"][0][1:-1] + "</td>"
+
+        # Prioritat
+        if "priority" in router_static[key]:
+            table_enr +=f"<td>" + router_static[key]["priority"][0] + "</td>"
+        else:
+            table_enr +="<td>-</td>"
+
+    snippet = set_var('defa', str(count))
+    snippet = set_var('table_enr', table_enr)
+
+    return snippet
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
@@ -44,6 +136,7 @@ if __name__ == "__main__":
         print(f"* -> optional")
         exit(1)
 
+    # loading html snippet
     snippet_path = "snippet.html"
     if len(sys.argv) == 3:
         snippet_path = sys.argv[2]
@@ -51,22 +144,13 @@ if __name__ == "__main__":
     f = open(snippet_path, "r")
     snippet = f.read()
 
+    # loading and parsing conf file
     conf_path = sys.argv[1]
     conf = load_conf(conf_path)
 
-    interf = conf['config system interface']
-    for key in interf.keys():
-        name = key[6:-1]
-        if name.startswith("port"):
-            print(f"----------------------")
-            print(f"interficie: {name}")
-            print(f"alias: " + interf[key]["alias"][0][1:-1])
-            print(f"ip: " + interf[key]["ip"][0])
-            if "dhcp-relay-ip" in interf[key]:
-                print(f"relay: " + interf[key]["dhcp-relay-ip"][0])
-            else:
-                print("relay: -")
+    snippet = fill_snippet(snippet, conf)
 
+    # saving output
     save_path = conf_path.split(".")[0] + ".html"
     print("Saving " + save_path)
     f = open(save_path, "w")
